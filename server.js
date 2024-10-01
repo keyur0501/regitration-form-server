85952303dcecc2e8724ddd961c5f9d9a17616def
amitdwivedi01 <shyamdwivedi595@gmail.com>
2024-10-01 16:44:49 +0530

    cors changed to all

--- server.js
+++ server.js
@@ -10,11 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRouter);
